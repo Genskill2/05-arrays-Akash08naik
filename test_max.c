c42c5cc594e087f7599de2020ef0109c9007189b
Akash08naik <84712144+Akash08naik@users.noreply.github.com>
2021-06-17 13:49:44 +0530

    Update test_max.c

--- test_max.c
+++ test_max.c
@@ -12,3 +12,35 @@ int main(void) {
   assert (max(y, 1) == 5);
   printf("Max: passed\n");
   }
+/*void int max(int x[],int count)
+{
+  for(int i;i<count;i++)
+  {
+    for(int j;j<count-1-j;j++)
+    {
+      if(x[j]>x[j+1])
+      {
+        int z=x[j+1];
+        x[j+1]=x[j];
+        x[j]=z;
+      }
+    }
+  }
+  return x[cout-1];*/
+}
+void int max(int y[],int count)
+{
+  for(int a;a<count;a++)
+  {
+    for(int a;b<count-1-a;b++)
+    {
+      if(y[b]>y[b+1])
+      {
+        int c=y[b+1];
+        y[b+1]=y[b];
+        y[b]=c;
+      }
+    }
+  }
+  return y[count-1];
+}
