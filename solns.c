fa96278b7631f695e79c5f77cd79eac6bafce196
Akash08naik <84712144+Akash08naik@users.noreply.github.com>
2021-06-18 21:52:52 +0530

    Update solns.c

--- solns.c
+++ solns.c
@@ -1,2 +1,20 @@
 /* Enter your solutions in this file */
 #include <stdio.h>
+
+int max(int arr[],int count)
+{int m;
+  for(int a;a<count;a++)
+  {
+    for(int b;b<count-1-a;b++)
+    {
+      if(arr[i]>arr[i+1])
+      {
+        m=arr[b+1];
+        arr[b+1]=arr[b];
+        arr[b]=m;
+      }
+    }
+  }
+ int c=arr[count-1];
+ return c;
+}
