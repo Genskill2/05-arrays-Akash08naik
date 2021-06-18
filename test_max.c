fe6347007268ef0eb32a33c7d45a13cb0b5cbee8
Akash08naik <84712144+Akash08naik@users.noreply.github.com>
2021-06-18 20:40:47 +0530

    Update test_max.c

--- test_max.c
+++ test_max.c
@@ -12,12 +12,17 @@ int main(void) {
   assert (max(y, 1) == 5);
   printf("Max: passed\n");
   }
-int max(const int arr[],int n){
-	int Max=arr[0];
-	for(int i=0;i<n;i++){
-		if(arr[i]>Max){
-			Max=arr[i];
+int max(int arr[],int n){
+	int Max;
+	for (int j=0;j<n;j++)
+	{
+	for(int i=0;i<n-1-j;i++){
+		if(arr[i]>arr[i+1]){
+			Max=arr[i+1];
+			arr[i+1]=arr[i];
+			arr[i]=Max;
 		}
+	}
 	}
 	return Max;
 }
