34917a483f343084619f3a3d51b9be64347294a9
Akash08naik <84712144+Akash08naik@users.noreply.github.com>
2021-06-19 12:39:22 +0530

    Update solns.c

--- solns.c
+++ solns.c
@@ -1,98 +1,70 @@
 /* Enter your solutions in this file */
 #include <stdio.h>
-#include<assert.h>
-
-
-int max(int max[],int a){
-  int i;
-  for(i=0;i<a;i++){
-  max[0]=max[0]>max[i]?(max[0]):(max[i]);
+int max(int a[], int n){
+  
+  int i,x=a[0];
+  for(i=0;i<n;i++)
+  {
+      if(x<a[i])
+       x=a[i];
   }
-  return max[0];}
-
-int min(const int arr[],int n){
-	int M=arr[0];
-	for(int i=0;i<n;i++){
-		if(arr[i]<M){
-			M=arr[i];
-		}
-	}
-	return M;
+   return x; 
 }
-
-float average(const int arr[],int n){
-	float sum=0.0;
-	for(int i=0;i<n;i++){
-		sum+=arr[i];
-	}
-	return sum/(float)n;
+int min(int a[], int n){
+  int i,x=a[0];
+  for(i=0;i<n;i++)
+  {
+      if(x>a[i])
+       x=a[i];
+    }
+   return x;  
 }
-
-
-int mode(int arr[],int n){
-	int num,temp_num;
-	int count=0,temp_count=0;
-	// sort(arr,n);
-	int temp;
-	for(int i=0;i<n-1;i++){
-		for(int j=0;j<n-1;j++){
-			if(arr[j+1]<arr[j]){
-				temp=arr[j];
-				arr[j]=arr[j+1];
-				arr[j+1]=temp;
-			}
-		}
-	}
-	temp_num=arr[0];
-	for(int i=0;i<n;i++){
-		if(arr[i]==temp_num){
-			temp_count++;
-		}
-		else{
-			if(temp_count>count){
-				count=temp_count;
-				num=temp_num;
-			}
-			temp_num=arr[i];
-			temp_count=1;
-		}
-	}
-	if(temp_count>count){
-		count=temp_count;
-		num=temp_num;
-	}
-	return num;
-}
-
-
-
-int factors(int num,int arr[]){
-	int prime=2,ref=0;
-	int count=0,index=0;
-	while(num!=1){
-		if(num%prime==0){
-			num=num/prime;
-			count++;
-			arr[index]=prime;
-			index++;
-		}
-		else{
-			while(1){
-				prime++;
-				for(int j=2;j<prime;j++){
-					if(prime%j==0)
-						ref++;
-				}
-				if(ref==0){
-					break;
-				}
-				else{
-					ref=0;
-				}
-			}
-		}
-	}
-	return count;
+float average(int a[], int n){ 
+   int i ; float sum=0;
+   for(i=0;i<n;i++)
+  {
+    sum+=a[i]; 
+  }
+  sum=sum/n;
+  return sum;
+  }
+int mode(int a[] , int n){
+  int i,j ;
+  int maxcount=0 , maxvalue=0;
+  for(i=0 ; i<n; ++i){
+    int count = 0;
+    for(j=0; j<n ; ++j){
+      if(a[i]==a[j])
+        ++count ;
+    }
+    
+    if(count > maxcount){
+      maxcount = count;
+      maxvalue = a[i];
+    }
+  }
+   return maxvalue;
+ }
+int factors(int n, int a[]){
+  
+  int i, j=0;
+  int x=n;
+  for(i=2;i<=n;i++)
+  {
+    
+    if(n%i==0)
+    {
+      
+      a[j]=i;
+     // printf("%d %d %d\n",j,n,a[j]);
+      n=n/i;
+      
+      i=1;
+      j++;
+      
+    } 
+  }
+  return j;  
 }
 
 
